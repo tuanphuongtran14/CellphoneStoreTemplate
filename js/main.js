ddd220d25fcd6738ec85f78e9fe52cd3203d2a27
Phuong <tuanphuongtran.14@gmail.com>
2021-05-25 10:59:06 +0700

    Add product page

--- js/main.js
+++ js/main.js
@@ -92,6 +92,11 @@ function selectByThumbnail() {
     var thumbnails = document.getElementsByClassName('thumbnail');
     var flkty = new Flickity('.product-images__slide');
 
+    flkty.on( 'change', function(index) {
+        document.querySelector(".thumbnail.active").classList.remove("active");
+        thumbnails[index].classList.add("active");
+    });
+
     for(let i = 0; i < thumbnails.length; i++) {
         thumbnails[i].onclick = function() {
             document.querySelector(".thumbnail.active").classList.remove("active");
