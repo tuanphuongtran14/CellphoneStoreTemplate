6108a6041d4b624337b3b29c5f2f4bcd079aee78
Nhat Phi <nhatphi2106@gmail.com>
2021-06-15 09:54:49 +0700

    add some js to payment page

--- js/payment.js
+++ js/payment.js
@@ -1,3 +1,4 @@
+//Function để lấy vị trí hiện tại của item trong class
 function indexInClass(collection, node) {
     for (var i = 0; i < collection.length; i++) {
       if (collection[i] === node)
@@ -10,18 +11,27 @@ var quantityProducts = document.getElementsByClassName('ml-2 quantity__input');
 var priceProducts= document.getElementsByClassName('card-text item-price');
 var test=document.getElementById('price1');
 
+//xử lý việc tăng giảm giá tiền khi đổi số lượng
 for (var i=0; i<quantityProducts.length;i++)
 {
     quantityProducts[i].oninput = function(e) {
     
-        console.log(indexInClass(quantityProducts,e.target));
         index=indexInClass(quantityProducts,e.target);
-        var price= 8990000;
+        var price= 8990000; // giá lấy từ data
         price=price*quantityProducts[index].value;
         textPrice= (price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.'); 
         textPrice=textPrice.slice(0,-3)+"₫";
-        console.log(textPrice);
         priceProducts[index].textContent=textPrice;
-        
+        var total =0;
+
+        for (var j=0; j<quantityProducts.length;j++)
+        {
+            total = total+parseInt(quantityProducts[j].value);
+
+        }
+        total=total*8990000;
+        textTotal=(total).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.'); 
+        textTotal=textTotal.slice(0,-3)+"₫";
+        document.getElementsByClassName('total-price')[0].textContent=textTotal;
     }
 }
